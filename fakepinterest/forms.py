cc4e91783f74d323a03d584b7fd386901ca9c16b
AlexSilva91 <alexsilvaalves17@outlook.com>
2024-04-18 22:09:45 -0300

    projeto concluido

---
+++ fakepinterest/forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import  StringField, PasswordField, SubmitField, FileField
+from wtforms.validators import  DataRequired, Email, EqualTo, length, ValidationError
+from fakepinterest.models import Usuario
+
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired()])
+    botao_confirmacao  = SubmitField("Fazer login")
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if not usuario:
+            raise ValidationError("Usuário não encontrado!")
+
+class FormCriarConta(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    username = StringField("Nome de usuário", validators=[DataRequired()])
+    senha = PasswordField("Senha", validators=[DataRequired(), length(6, 10)])
+    confirmar_senha = PasswordField("Confirmar senha", validators=[DataRequired(), EqualTo("senha")])
+    botao_confirmacao = SubmitField("Criar conta")
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            raise ValidationError("E-mail já cadastrado, faça login para poder continuar!")
+
+class FormFoto(FlaskForm):
+    foto = FileField("Foto", validators=[DataRequired()])
+    botao_confirmacao = SubmitField("Enviar")
