2afb31149b4d1bd2bd69c17e4bf139d9b663d933
AlexSilva91 <alexsilvaalves17@outlook.com>
2024-04-18 23:49:41 -0300

    ajustes

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,17 +1,11 @@
-import os
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
-if os.getenv('DEBUG') == 0:
-    link_banco = os.getenv("DATABASE_URL")
-else:
-    link_banco = "sqlite:///comunidade.db"
-
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('git')
 app.config["SECRET_KEY"] = "4f52e669753727026e0c70c3d2433973"
 app.config["UPLOAD_FOLDER"] = "static/fotos_post"
 
