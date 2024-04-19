947ffc756000f4b45082cfc7c9773eb1ad0620e7
AlexSilva91 <alexsilvaalves17@outlook.com>
2024-04-19 00:00:14 -0300

    ajustes

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -4,12 +4,13 @@ from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 import os
 
+database = SQLAlchemy()
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('git')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
 app.config["SECRET_KEY"] = "4f52e669753727026e0c70c3d2433973"
 app.config["UPLOAD_FOLDER"] = "static/fotos_post"
 
-database = SQLAlchemy(app)
+database.init_app(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "homepage"
